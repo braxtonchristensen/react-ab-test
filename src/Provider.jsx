e8de623380aa694963012bdc23cf61f97dccf0d1
Braxton M. Christensen <braxton.christensen@gmail.com>
2019-12-17 14:03:52 -0700

    start provider

---
+++ src/Provider.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+let abContext;
+
+function getABContext() {
+  if (!abContext) {
+    abContext = React.createContext({});
+  }
+
+  return abContext;
+}
+
+export const ABConsumer = props => {
+  const ABContext = getABContext();
+  return (
+    <ABContext.Consumer>
+      {context => props.children(context.client)}
+    </ABContext.Consumer>
+  );
+};
+
+export const ABProvider = ({ store, children }) => {
+  const ABContext = getABContext();
+  return (
+    <ABContext.Consumer>
+      {(context = {}) => {
+        if (store && context.store !== store) {
+          context = Object.assign({}, context, { store });
+        }
+
+        return (
+          <ABContext.Provider value={context}>{children}</ABContext.Provider>
+        );
+      }}
+    </ABContext.Consumer>
+  );
+};
